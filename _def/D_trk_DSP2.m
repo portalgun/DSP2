6925087500acdd3d319aa1bf5e38636941ee3add
Dave White <d>
2021-03-29 10:39:53 -0400

    Add trk def

---
+++ _def/D_trk_DSP2.m
@@ -0,0 +1,25 @@
+hashAlias='DSP2_lim_dvn';
+prjCode='DSP2';
+imgDTB='LRSI';
+natORflt='nat';
+imgDim='2D';
+method='2IFC';
+prjInd=[1 1];
+pass=1;
+subjs={'DNW','JDB'};
+pAuthor='DNW';
+description='';
+language='matlab';
+expHost='wheatstone';
+Xname={'disparity','disparity contrast'};
+Xunits={'arcmin','au'};
+description='Disparity contrast experiments, binned by disparity contrast';
+
+rnd_bBlk=1;
+rnd_bTrl=0;
+rnd_bIntrvl=0;
+
+
+% MAP
+hashes=imapCommon.alias2hashes(hashAlias,imgDTB);
+% XXX MAKE UPPER CASE
